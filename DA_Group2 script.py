819dae8a1820f3e1bf373e2073802d27b86bca1c
Jimmy5115 <85581824+Jimmy5115@users.noreply.github.com>
2021-08-29 05:36:23 +0800

    Update DA_Group2 script.py

--- DA_Group2 script.py
+++ DA_Group2 script.py
@@ -1,5 +1,6 @@
 # Use the Request library
 import requests
+
 # Set the target webpage
 url = "https://brickset.com/sets/year-1999"
 r = requests.get(url)
@@ -13,7 +14,7 @@ print("**********")
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
 # This will modify the headers user-agent
-headers = { 'User-Agent' : "Mobile" }
+headers = {'User-Agent': "Mobile"}
 # Test it on an external site
 url2 = 'http://httpbin.org/headers'
 rh = requests.get(url2, headers=headers)
@@ -23,58 +24,41 @@ print("**********")
 print("Status code:")
 print("\t *", r.status_code)
 
-#use scrapy web crawler search for image links in the website
+# use scrapy web crawler search for image links in the website
 import scrapy
 
+
 class NewSpider(scrapy.Spider):
+    name = "new_spider"
 
- name = "new_spider"
+    start_urls = ['https://brickset.com/sets/year-1999']
 
- start_urls = ['https://brickset.com/sets/year-1999']
+    def parse(self, response, **kwargs):
+
+        xpath_selector = '//img'
+
+        for x in response.xpath(xpath_selector):
+            newsel = '@src'
+
+            yield {
+
+                'Image Link': x.xpath(newsel).extract_first(),
+
+            }
+
+        # To recurse next page
+
+        Page_selector = '.next a ::attr(href)'
+
+        next_page = response.css(Page_selector).extract_first()
+
+        if next_page:
+            yield scrapy.Request(
+
+                response.urljoin(next_page),
+
+                callback=self.parse
+
+            )
 
 
- def parse(self, response, **kwargs):
-
-  xpath_selector = '//img'
-
-
-  for x in response.xpath(xpath_selector):
-
-    newsel = '@src'
-
-    yield {
-
-      'Image Link': x.xpath(newsel).extract_first(),
-
-}
-
-# To recurse next page
-
-  Page_selector = '.next a ::attr(href)'
-
-  next_page = response.css(Page_selector).extract_first()
-
-  if next_page:
-
-     yield scrapy.Request(
-
-       response.urljoin(next_page),
-
-       callback=self.parse
-
- )
-
-import requests
-
-class TestApplication(request.TestCase):
-
-	def test_upper(self):
-		self.asserEqual('foo'.upper(),'FOO')
-
-	
-	def test_isupper(self):
-		self.assertTrue('FOO'.isupper())
-		self.assertFalse('Foo'.isupper())
-
-if __name__== '__main__':
-	requests.main()
