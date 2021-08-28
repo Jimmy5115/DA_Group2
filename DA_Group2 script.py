5b250088d9673799d204fa0a4c28f7815b79ab7b
Jimmy5115 <85581824+Jimmy5115@users.noreply.github.com>
2021-08-28 13:24:26 +0800

    Update DA_Group2 script.py

--- DA_Group2 script.py
+++ DA_Group2 script.py
@@ -25,6 +25,47 @@ print("**********")
 print("Status code:")
 print("\t *", r.status_code)
 
+#scrappy attempt (0 img scrapped)
+import scrapy
+
+class NewSpider(scrapy.Spider):
+
+ name = "new_spider"
+
+ start_urls = ['https://brickset.com/sets/year-1999']
+
+
+ def parse(self, response, **kwargs):
+
+  xpath_selector = '//img'
+
+
+  for x in response.xpath(xpath_selector):
+
+    newsel = '@src'
+
+    yield {
+
+      'Image Link': x.xpath(newsel).extract_first(),
+
+}
+
+# To recurse next page
+
+  Page_selector = '.next a ::attr(href)'
+
+  next_page = response.css(Page_selector).extract_first()
+
+  if next_page:
+
+     yield scrapy.Request(
+
+       response.urljoin(next_page),
+
+       callback=self.parse
+
+ )
+
 
 
 
